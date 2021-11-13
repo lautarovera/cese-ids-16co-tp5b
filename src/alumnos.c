bd95aed0abef1ed9188643eef48f4ac79cdef84c
Lautaro Vera <lautarovera93@gmail.com>
2021-11-13 13:21:42 -0300

    Se agregan los datos del alumno VERA, Lautaro

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s LAUTARO_VERA = {
+    .apellidos = "VERA",
+    .nombres = "Lautaro Juan Bautista",
+    .documento = "37.585.785",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &LAUTARO_VERA,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
